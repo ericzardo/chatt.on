5bd7de44981cde22c8ddcb8d83419bd597615446
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2024-11-02 22:23:05 -0300

    add barear token

--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -1,11 +1,21 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:1337";
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+console.log("Base URL configurada:", baseURL);
 
 const api = axios.create({
   baseURL,
-  withCredentials: true,
-  headers: { "Access-Control-Allow-Origin": "*" }
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 api.interceptors.response.use(
